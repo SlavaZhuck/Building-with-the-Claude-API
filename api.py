aca175ea9da2701e7960c53d063e7fa6627953c5
SlavaZhuck <zhukviachaslau@gmail.com>
2026-09-24 11:59:31 +0300

    added system prompt

--- api.py
+++ api.py
@@ -5,6 +5,11 @@ from anthropic import Anthropic
 
 client = Anthropic()
 model = "claude-sonnet-4-5"
+system_prompt = """
+You are a patient math tutor.
+Do not directly answer a student's questions.
+Guide them to a solution step by step.
+"""
 
 def add_user_message(messages, text):
     user_message = {"role": "user", "content": text}
@@ -14,12 +19,18 @@ def add_assistant_message(messages, text):
     assistant_message = {"role": "assistant", "content": text}
     messages.append(assistant_message)
 
-def chat(messages):
-    message = client.messages.create(
-        model=model,
-        max_tokens=1000,
-        messages=messages,
-    )
+
+def chat(messages, system=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+    }
+
+    if system:
+        params["system"] = system
+
+    message = client.messages.create(**params)
     return message.content[0].text
 
 messages = []
@@ -29,7 +40,7 @@ while True:
     # print(">", user_input)
     add_user_message(messages, user_input)
 
-    answer=chat(messages)
+    answer=chat(messages, system_prompt)
     add_assistant_message(messages, answer)
     print("---")
     print(answer)
